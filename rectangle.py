799b39a0590036b512f81261ea243bef901fedf5
tmilang <talang@uos.de>
2016-04-26 23:05:33 +0200

    Added files via upload

---
+++ rectangle.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.home()
+
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(200)
+
+input("press enter to exit")
---
+++ triangle.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.home()
+
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+
+input("press enter to exit")
